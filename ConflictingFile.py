4b3d8d183f0c209854150664e3fca1df830412e1
cmvalma <cmvalma.rg@gmail.com>
2018-10-17 03:02:18 +0200

    .py modificado

--- ConflictingFile.py
+++ ConflictingFile.py
@@ -1,3 +1,3 @@
 # Imprime un texto - En el original tiene la palabra "ORIGINAL" 
 # y en el cambiado tiene la palabra "CAMBIADO" para gestionar el conflicto
-print("Este es el documento ORIGINAL")
+print("Este es el documento CAMBIADO")
